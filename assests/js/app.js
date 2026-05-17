3deee6785b661fd5e2f3c676bf5076fbca17931d
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-05-17 18:31:53 +0530

    work on read functionality

---
+++ assests/js/app.js
@@ -0,0 +1,35 @@
+const todocontainer = document.getElementById('todocontainer')
+
+
+let todoArr = [
+  {
+    todoItem: "CSS",
+    todoId: "2qwe12-231-231wd-ew112e",
+  },
+  {
+    todoItem: "JS & ES6",
+    todoId: "2qwe12-231-231wd-ew1133",
+  },
+  {
+    todoItem: "HTML",
+    todoId: "2qwe12-231-231wd-ew11563",
+  },
+];
+
+function templating(arr){
+  let result =``
+
+  arr.forEach(ele=>{
+    result+=`<li class="list-group-item d-flex justify-content-between" id='${ele.todoId}'>
+              <strong>${ele.todoItem}</strong>
+              <div>
+                <i class="fa-regular fa-pen-to-square fa-2x text-primary" onclick="OnEdit(this)"></i>
+                <i class="fa-solid fa-trash fa-2x text-danger" onclick="Onremove(this)"></i>
+              </div>
+            </li>`
+  })
+
+  todocontainer.innerHTML =result;
+}
+
+templating(todoArr)
